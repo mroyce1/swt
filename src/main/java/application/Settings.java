eb74a484a41127d74523245440da24d0a5b513f4
闫赫辰 <hechenyan@yanhechendeMacBook-Pro-2.local>
2019-10-24 17:44:14 +0200

    add riversand wikidata

--- src/main/java/application/Settings.java
+++ src/main/java/application/Settings.java
@@ -49,9 +49,48 @@ public class Settings {
             " ?animal rdfs:label \"Tiger\"@en.\n" +
             " }\n";
 
-    public static final String countries = "bla";
-    public static final String rivers = "bla";
+    //public static final String countries = "bla";
+    
+    //dbpedia query of River
+    public static final String rivers = "PREFIX dbr: <http://dbpedia.org/resource/>\n"+
+    		"PREFIX dbo: <http://dbpedia.org/ontology/>\n"+
+    		"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
+    		"SELECT DISTINCT ?riverName WHERE {\n"+
+    		"?a rdfs:label ?riverName.\n"+
+    		"?an a dbo:River.\n"+
+    		"FILTER (lang(?riverName) = 'en')\n"
+    		" FILTER( regex(str(?riverName) , \"^B\"))\n"+
+    		" }\n" +
+            " LIMIT 100\n";
+    
+    public static final String AskValidateRiver ="PREFIX dbr: <http://dbpedia.org/resource/>\n"+
+    		"PREFIX dbo: <http://dbpedia.org/ontology/>\n"+
+    		"PREFIX foaf: <http://xmlns.com/foaf/0.1/>"+
+    		"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+    		"ASK\n"+
+    		"{?c foaf:name \"Boga River\"@en.\n"+
+    		"?c a dbo:River.}\n"
+    		
+    		//widata query of River
+    		public static final String Rivers="PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
+			"PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+			"PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+			"PREFIX wd: <http://www.wikidata.org/entity/>\n" +
+			"SELECT DISTINCT ?riverLabel\n" +
+			"WHERE \n" +
+			"{\n" +
+			"  {?river wdt:P30 wd:Q46.}\n" +
+			"  UNION {?river wdt:P30 wd:Q48.}\n" +
+			"  UNION {?river wdt:P30 wd:Q15.}\n" +
+			"  UNION {?river wdt:P30 wd:Q18.}\n" +
+			"  UNION {?river wdt:P30 wd:Q49.}\n" +
+			"  UNION {?river wdt:P30 wd:Q538}\n" +
+			"  UNION {?river wdt:P30 wd:Q51.}\n" +
+			"  ?river wdt:P31 wd:Q4022;\n" +
+			"  SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". }\n" +
+			"}\n";	
 
 
     public static final String dbpediaEndpoint = "http://dbpedia.org/sparql";
+    public static final String wikidataEndpoint="https://query.wikidata.org/sparql";
 }
