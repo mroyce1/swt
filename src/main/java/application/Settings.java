421c255636432a068600475ab67f837ac689c95e
basil099 <basilsattler@gmx.de>
2019-10-24 10:10:58 +0200

    added animal Queries .

--- src/main/java/application/Settings.java
+++ src/main/java/application/Settings.java
@@ -21,6 +21,17 @@ public class Settings {
             " }\n" +
             " LIMIT 100\n";
 
+    public static final String animalsDBPedia = "PREFIX dbr: <http://dbpedia.org/resource/>\n" +
+            "PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
+            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+            "SELECT DISTINCT ?animalName WHERE {\n" +
+            " ?an rdfs:label ?animalName.\n" +
+            " ?an a dbo:Animal.\n" +
+            " FILTER( regex(str(?animalName) , \"^B\"))\n" +
+            " FILTER (lang(?animalName) = 'en')\n" +
+            " }\n" +
+            " LIMIT 100\n";
+
     public static final String doesCompanyExistQuery = "PREFIX dbr: <http://dbpedia.org/resource/>\n" +
             "PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
             "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n"+
@@ -28,6 +39,14 @@ public class Settings {
             "ASK {\n" +
             " ?c foaf:name \"Adidas\"@en.\n" +
             " ?c a dbo:Company.\n" +
+            " }\n";
+
+    public static final String doesAnimalExistQuery = "PREFIX dbr: <http://dbpedia.org/resource/>\n" +
+            "PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
+            "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+            "ASK {\n" +
+            " ?animal rdfs:label \"Tiger\"@en.\n" +
             " }\n";
 
     public static final String countries = "bla";
